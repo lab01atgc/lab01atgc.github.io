795431bd0c575c0ab94045edd507fbc21aace26a
lab01atgc <covadiuc@uw.edu>
2024-02-11 18:36:05 -0800

    init

---
+++ CS224W _ Home_files/Xheader.js
@@ -0,0 +1,43 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="index.html">CS224W</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="index.html#content">Content</a></li>\
+              <li><a href="index.html#schedule">Schedule</a></li>\
+              <li><a href="info.html">Course Info</a></li>\
+              <li><a href="projects.html">Projects</a></li>\
+              <li><a href="oh.html">Office Hours</a></li>\
+              <li><a href="faq.html">FAQ</a></li>\
+              <li><a href="https://www.gradescope.com/courses/631028">Gradescope</a></li>\
+              <li><a href="https://canvas.stanford.edu/courses/179477">Canvas</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="http://snap.stanford.edu/">\
+    <img src="images/snaplogo.png" class="logo-left">\
+  </a>\
+  <a href="http://stanford.edu/">\
+    <img src="images/stanfordlogo.png" class="logo-right">\
+  </a>\
+  <h1>CS224W: Machine Learning with Graphs</h1>\
+  <h3>Stanford / Fall 2023</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
